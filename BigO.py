8730a2c88a8101d4af1e7297cb822a3e5586ed6a
jj888 <jaconjcondes@gmail.com>
2019-10-26 21:10:59 -0700

    init bio O

--- BigO.py
+++ BigO.py
@@ -1,1 +1,7 @@
-print("Hello World")
+"""
+What?
+Big O time is the language and metric we use to describe the efficiency of algorithms
+
+Why?
+Big O notation allows you to analyze algorithms in terms of overall efficiency and scaleability
+"""
